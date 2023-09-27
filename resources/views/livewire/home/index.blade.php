eae3bbe253b686f7c439a557771c5da150ce61e6
Fabiano Fernandes <contato@fabianofernandes>
2023-09-27 14:55:04 -0300

    Logo na home page

---
+++ resources/views/livewire/home/index.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <div class="flex h-screen w-full bg-white justify-center items-center">
+        <img src="{{ asset('images/logo_450x172.png') }}" alt="">
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Home\Index as Home;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('lading_page');
-});
+Route::get('/', Home::class)->name('lading.page');
