9bacfd9313d3ff3d1823f92d91ff857284db0f30
Fabiano Fernandes <contato@fabianofernandes>
2023-09-30 18:27:50 -0300

    fix: ajuste no model Newsletter

--- app/Models/Newsletter.php
+++ app/Models/Newsletter.php
@@ -2,20 +2,8 @@
 
 namespace App\Models;
 
-<<<<<<< HEAD
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
-class Newsletter extends Model
-{
-    use HasFactory;
-=======
-use DateTime;
-use Carbon\CarbonImmutable;
-use WendellAdriel\Lift\Lift;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Newsletter extends Model
 {
@@ -31,5 +19,4 @@ class Newsletter extends Model
 
     #[Config(fillable: true, rules: ['required', 'string'])]
     public string $phone;
->>>>>>> 1147288 (refactor: remoção de comentários)
 }
