86865d210e56cd410e0bd7a32149be0ec23ec35d
Fabiano Fernandes <contato@fabianofernandes>
2023-09-30 18:31:22 -0300

    fix: add import softdelete in newsletter model

--- app/Models/Newsletter.php
+++ app/Models/Newsletter.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use WendellAdriel\Lift\Lift;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Newsletter extends Model
