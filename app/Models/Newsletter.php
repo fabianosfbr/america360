bea3f178c36f457509467480a37561779f7fe853
Fabiano Fernandes <contato@fabianofernandes>
2023-09-30 18:30:40 -0300

    fix: add import lavarel lift

--- app/Models/Newsletter.php
+++ app/Models/Newsletter.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use WendellAdriel\Lift\Lift;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Newsletter extends Model
 {
