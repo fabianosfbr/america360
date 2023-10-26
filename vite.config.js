6837748f09fcd083dd7d852a3c20eb58ca6d2297
Fabiano Fernandes <contato@fabianofernandes>
2023-10-26 15:05:56 -0300

    style: generate new assets

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/scss/app.scss',
                 'resources/css/filament/admin/theme.css',
             ],
             refresh: [
